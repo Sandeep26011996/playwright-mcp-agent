7c7a102eb4e7dc17cdeab743f31142e86cea6693
sandeep26011996 <sandeepchaudhary336@gmail.com>
2026-03-14 22:22:14 +0530

    smoke test cases added

--- tests/smoke/flipkartOfficeChairs.spec.ts
+++ tests/smoke/flipkartOfficeChairs.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@playwright/test';
-import { FlipkartHomePage } from './pages/FlipkartHomePage';
+import { FlipkartHomePage } from '../pages/FlipkartHomePage';
 
-test('Flipkart search for office chairs', async ({ page }) => {
+test('Flipkart search for office chairs @smoke', async ({ page }) => {
   const flipkart = new FlipkartHomePage(page);
   await flipkart.goto();
   await flipkart.search('office chairs');
