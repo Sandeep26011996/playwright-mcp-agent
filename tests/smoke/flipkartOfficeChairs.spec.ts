ea607bf98f483f58fe6cc96562eeff12ebf3a177
sandeep26011996 <sandeepchaudhary336@gmail.com>
2026-03-14 23:17:46 +0530

    smoke test case folder added

--- tests/smoke/flipkartOfficeChairs.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test';
-import { FlipkartHomePage } from '../pages/FlipkartHomePage';
-
-test('Flipkart search for office chairs @smoke', async ({ page }) => {
-  const flipkart = new FlipkartHomePage(page);
-  await flipkart.goto();
-  await flipkart.search('office chairs');
-});
